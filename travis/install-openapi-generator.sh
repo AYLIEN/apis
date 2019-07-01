4c146d80883c6cb49ade55e4e7dde95d14e4f7b5
Mahdi Dibaiee <mdibaiee@aol.com>
2019-07-01 17:46:04 +0430

    feat(sdk): language-specific configurations

--- travis/install-openapi-generator.sh
+++ travis/install-openapi-generator.sh
@@ -4,7 +4,8 @@ OPENAPI_DIR=./openapi-codegen
 OPENAPI_PATCHES_DIR=`pwd`/travis-ci/openapi-codegen-patches
 
 echo "Downloading openapi-generator-cli..."
-wget http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/3.3.4/openapi-generator-cli-3.3.4.jar -O openapi-generator-cli.jar
+VERSION=4.0.2
+wget http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/$VERSION/openapi-generator-cli-$VERSION.jar -O openapi-generator-cli.jar
 
 # set $OPENAPI_CMD
 export OPENAPI_CMD="java -jar $(pwd)/openapi-generator-cli.jar"
