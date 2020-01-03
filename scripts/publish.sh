7c03fba00f9934fc13f863e1c288d9921e8a0080
Mahdi Dibaiee <mdibaiee@pm.me>
2020-01-03 16:08:31 +0000

    feat(publish): automatic pushing to git and better cleanup

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,20 @@
 cd ../sdks/news-api
 
+release_note=${1:-"minor update"}
+
+function git_push {
+  rm -rf .git
+  git init
+  git add .
+  git commit -m "$release_note"
+  git remote add origin git@github.com:aylien/aylien_newsapi_$lang
+
+  git pull origin master -s recursive -X ours
+  git cherry-pick --skip
+  git rebase --continue
+  git push origin master
+}
+
 echo "javascript: publishing to npm"
 if [ ! -f "$HOME/.npmrc" ]; then
   echo "- $HOME/.npmrc not found, using NPM_AUTH_TOKEN from env to create it."
@@ -7,15 +22,21 @@ if [ ! -f "$HOME/.npmrc" ]; then
 fi
 
 cd javascript
+lang=nodejs
+git_push
+
 npm install
 npm run build
 npm publish
 cd ..
 
 echo "php: publishing to packagist"
-echo "- please note that packagist reads the actual code from the git repository directly, so this step must be run after the changes are already pushed on github"
+echo "- please note that packagist expects a new github release, and only updates the package in that case"
 echo "- using PACKAGIST_API_TOKEN from env"
 cd php
+lang=php
+git_push
+
 curl -XPOST -H 'content-type:application/json' \
   "https://packagist.org/api/update-package?username=aylien&apiToken=$PACKAGIST_API_TOKEN" \
   -d '{"repository":{"url":"https://github.com/aylien/aylien_newsapi_php"}}'
@@ -24,6 +45,9 @@ cd ..
 echo "python: publishing to pypi"
 echo "- using PYPI_USERNAME and PYPI_PASSWORD env variables"
 cd python
+lang=python
+git_push
+
 pip install twine
 python setup.py sdist bdist_wheel
 twine check dist/*
@@ -37,8 +61,27 @@ if [ ! -f "$HOME/.gem/credentials" ]; then
   echo ":rubygems_api_key: $GEM_API_KEY" > $HOME/.gem/credentials
 fi
 cd ruby
+lang=ruby
+git_push
+
 gem build aylien_news_api.gemspec
 gem push aylien_news_api-*.gem
+cd ..
 
-echo "go: there is no publication necessary, users pull directly from github"
+echo "go: there is no publication necessary, users pull directly from github."
+cd go
+lang=go
+git_push
+cd ..
 
+echo "java: TBD"
+cd java
+lang=java
+git_push
+cd ..
+
+echo "csharp: TBD"
+cd csharp
+lang=csharp
+git_push
+cd ..
