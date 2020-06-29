19f0b69acf1e20941372c56ef1c85389ddc80edc
Mahdi Dibaiee <mdibaiee@pm.me>
2020-06-29 10:54:02 +0100

    fix(post-gen): macOS command gsed

--- scripts/post-generation-modification.sh
+++ scripts/post-generation-modification.sh
@@ -1,25 +1,32 @@
 scripts=$PWD
 cd ../sdks/news-api
 
+OS=`uname`
+cmd=sed
+if [ "$OS" = 'Darwin' ]; then
+    # for MacOS
+    cmd=gsed
+fi
+
 cd javascript
 npm install
-sed -i 's/\(\w\+\)2/not\u\1/g' src/api/DefaultApi.js
-sed -i 's/\(\w\+\)2/not\u\1/g' docs/DefaultApi.md
+$cmd -i 's/\(\w\+\)2/not\u\1/g' src/api/DefaultApi.js
+$cmd -i 's/\(\w\+\)2/not\u\1/g' docs/DefaultApi.md
 npm run build
 cd ..
 
 cd go
-sed -i 's/\(\w\+\)2\b/Not\u\1/g' api_default.go
-sed -i 's/\(\w\+\)2\b/Not\u\1/g' docs/DefaultApi.md
-sed -i 's/NotInt3/Int32/g' api_default.go
-sed -i 's/Not42/422/g' api_default.go
-sed -i 's/GIT_USER_ID/aylien/' go.mod
-sed -i 's/GIT_REPO_ID/aylien_newsapi_go/' go.mod
+$cmd -i 's/\(\w\+\)2\b/Not\u\1/g' api_default.go
+$cmd -i 's/\(\w\+\)2\b/Not\u\1/g' docs/DefaultApi.md
+$cmd -i 's/NotInt3/Int32/g' api_default.go
+$cmd -i 's/Not42/422/g' api_default.go
+$cmd -i 's/GIT_USER_ID/aylien/' go.mod
+$cmd -i 's/GIT_REPO_ID/aylien_newsapi_go/' go.mod
 cd ..
 
 cd java
-sed -i 's/exclamation\(\w\+\)/not\u\1/g' src/main/java/com/aylien/newsapi/api/DefaultApi.java
-sed -i 's/exclamation\(\w\+\)/not\u\1/g' docs/DefaultApi.md
+$cmd -i 's/exclamation\(\w\+\)/not\u\1/g' src/main/java/com/aylien/newsapi/api/DefaultApi.java
+$cmd -i 's/exclamation\(\w\+\)/not\u\1/g' docs/DefaultApi.md
 rm -rf src/test
 python $scripts/xmlcombine.py pom.xml $scripts/java/distribution.xml >new-pom.xml
 rm pom.xml
@@ -27,30 +34,30 @@ mv new-pom.xml pom.xml
 cd ..
 
 cd php
-sed -i 's/\(\w\+\)2/not_\1/g' lib/Api/DefaultApi.php
-sed -i 's/not_42/422/g' lib/Api/DefaultApi.php
-sed -i 's/not_49/492/g' lib/Api/DefaultApi.php
-sed -i 's/\(\w\+\)2/not_\1/g' docs/Api/DefaultApi.md
-sed -i 's/GIT_USER_ID/aylien/' composer.json
-sed -i 's/GIT_REPO_ID/newsapi/' composer.json
+$cmd -i 's/\(\w\+\)2/not_\1/g' lib/Api/DefaultApi.php
+$cmd -i 's/not_42/422/g' lib/Api/DefaultApi.php
+$cmd -i 's/not_49/492/g' lib/Api/DefaultApi.php
+$cmd -i 's/\(\w\+\)2/not_\1/g' docs/Api/DefaultApi.md
+$cmd -i 's/GIT_USER_ID/aylien/' composer.json
+$cmd -i 's/GIT_REPO_ID/newsapi/' composer.json
 cd ..
 
 cd python
-sed -i 's/\(\w\+\)2/not_\1/g' aylien_news_api/api/default_api.py
-sed -i 's/\(\w\+\)2/not_\1/g' docs/DefaultApi.md
-sed -i 's/not_42/422/g' docs/DefaultApi.md
-sed -i 's/not_49/492/g' docs/DefaultApi.md
+$cmd -i 's/\(\w\+\)2/not_\1/g' aylien_news_api/api/default_api.py
+$cmd -i 's/\(\w\+\)2/not_\1/g' docs/DefaultApi.md
+$cmd -i 's/not_42/422/g' docs/DefaultApi.md
+$cmd -i 's/not_49/492/g' docs/DefaultApi.md
 cd ..
 
 cd ruby
-sed -i 's/@api_client.build_collection_param(\([^,]*\), :multi.*/\1 if !\1.nil?/g' lib/aylien_news_api/api/default_api.rb
-sed -i 's/\(\w\+\)\[\]/\1/g' lib/aylien_news_api/api/default_api.rb
-sed -i 's/\(\w\+\)2/not_\1/g' lib/aylien_news_api/api/default_api.rb
-sed -i 's/\(\w\+\)2/not_\1/g' docs/DefaultApi.md
+$cmd -i 's/@api_client.build_collection_param(\([^,]*\), :multi.*/\1 if !\1.nil?/g' lib/aylien_news_api/api/default_api.rb
+$cmd -i 's/\(\w\+\)\[\]/\1/g' lib/aylien_news_api/api/default_api.rb
+$cmd -i 's/\(\w\+\)2/not_\1/g' lib/aylien_news_api/api/default_api.rb
+$cmd -i 's/\(\w\+\)2/not_\1/g' docs/DefaultApi.md
 cd ..
 
 cd csharp
-sed -i 's/\(\w\+\)2/not\u\1/g' src/Aylien.NewsApi/Api/DefaultApi.cs
-sed -i 's/\(\w\+\)2/not\u\1/g' docs/DefaultApi.md
-sed -i 's/not42/422/g' docs/DefaultApi.md
-sed -i 's/not49/492/g' docs/DefaultApi.md
+$cmd -i 's/\(\w\+\)2/not\u\1/g' src/Aylien.NewsApi/Api/DefaultApi.cs
+$cmd -i 's/\(\w\+\)2/not\u\1/g' docs/DefaultApi.md
+$cmd -i 's/not42/422/g' docs/DefaultApi.md
+$cmd -i 's/not49/492/g' docs/DefaultApi.md
