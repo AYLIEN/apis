700d3f608e2fb5a97cf3a4640dc54562c42f5402
Marcin Jakubowski <mjakubowski84@gmail.com>
2021-08-04 10:01:59 +0200

    fix: upgrade openapi-generator

--- scripts/install-openapi-generator.sh
+++ scripts/install-openapi-generator.sh
@@ -5,7 +5,7 @@ OPENAPI_PATCHES_DIR=$(pwd)/travis-ci/openapi-codegen-patches
 
 echo "Downloading openapi-generator-cli..."
 #VERSION=4.3.0
-wget https://github.com/AYLIEN/openapi-generator/releases/download/4.3.1-aylien2/openapi-generator-cli.jar
+wget https://github.com/AYLIEN/openapi-generator/releases/download/5.2.0-aylien/openapi-generator-cli.jar
 
 # set $OPENAPI_CMD
 export OPENAPI_CMD="java -jar $(pwd)/openapi-generator-cli.jar"
